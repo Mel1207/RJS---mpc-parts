a3c4bae1f572e57f12d875a8e6f85db4b3eda483
Mel <meljohnpualon@gmail.com>
2021-09-17 06:08:11 +0800

    added new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './sass/main.css'
 import NavbarComponent from './components/navbar/NavbarComponent'
+import PcList from './components/PC-List/PcList'
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div>
         <NavbarComponent />
         app body
+        <PcList />
       </div>
     )
   }
