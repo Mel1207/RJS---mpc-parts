77fd8cc9d49273fa3d1b3ba27b2b9b39e51cac4b
Mel <meljohnpualon@gmail.com>
2021-09-18 07:23:33 +0800

    added state

--- src/components/PC-PartList/index.js
+++ src/components/PC-PartList/index.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import Parts from '../PC-Part/Parts'
+import { partsData } from '../../partsData'
 
 export default class PartsList extends Component {
+    state = {
+        parts: partsData
+    };
+
     render() {
+        console.log(this.state.parts)
+
         return (
             <div className="container">
                 <h1>this is Parts list container </h1>
--- src/partsData.js
+++ src/partsData.js
@@ -6,25 +6,25 @@ export const partsData = [
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
     },
     {
-        id: 1, 
+        id: 2, 
         partName: "Motherboard - Aorus B350M",
         img: "./img/processor.jpg",
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
     },
     {
-        id: 1, 
+        id: 3, 
         partName: "RAM - Tforce Delta RGB",
         img: "./img/processor.jpg",
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
     },
     {
-        id: 1, 
+        id: 4, 
         partName: "storage: samsung 1tb SSD",
         img: "./img/processor.jpg",
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
     },
     {
-        id: 1, 
+        id: 5, 
         partName: "Case - Aorus Xven Light Case",
         img: "./img/processor.jpg",
         info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
