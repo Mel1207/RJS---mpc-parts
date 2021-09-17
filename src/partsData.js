2c2757d05a147831262f4626ab796abaad402644
Mel <meljohnpualon@gmail.com>
2021-09-18 07:18:42 +0800

    added pc data

---
+++ src/partsData.js
@@ -0,0 +1,33 @@
+export const partsData = [
+    {
+        id: 1, 
+        partName: "Processor - Intel Core I7",
+        img: "./img/processor.jpg",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
+    },
+    {
+        id: 1, 
+        partName: "Motherboard - Aorus B350M",
+        img: "./img/processor.jpg",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
+    },
+    {
+        id: 1, 
+        partName: "RAM - Tforce Delta RGB",
+        img: "./img/processor.jpg",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
+    },
+    {
+        id: 1, 
+        partName: "storage: samsung 1tb SSD",
+        img: "./img/processor.jpg",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
+    },
+    {
+        id: 1, 
+        partName: "Case - Aorus Xven Light Case",
+        img: "./img/processor.jpg",
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam earum vel illo nulla suscipit eveniet possimus accusamus quibusdam, itaque repellendus ex consequatur. Omnis, impedit itaque! Vitae distinctio quaerat temporibus?"
+    },
+    
+]
