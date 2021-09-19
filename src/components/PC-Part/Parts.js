f87c3e919a5a283d55eab5e84def9e260f50c9f0
Mel <meljohnpualon@gmail.com>
2021-09-19 19:08:10 +0800

    fix card box styling

--- src/components/PC-Part/Parts.js
+++ src/components/PC-Part/Parts.js
@@ -9,8 +9,15 @@ export default class Parts extends Component {
 
         return (
             <div className="card-part-item">
-                <div className="img-container">
+                <div className="img-container" id={id}>
                     <img src={img} alt="" />
+                    <span className="close-btn">x</span>
+
+                    <div className="item-info">
+                        <h4>{partName}</h4>
+                        <span className="info-toggle-btn">info</span>
+                        <p>{info}</p>
+                    </div>
                 </div>
             </div>
         )
