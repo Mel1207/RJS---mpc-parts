9715952b044e40788803e1b3d018db66fc0c6a0e
Mel <meljohnpualon@gmail.com>
2021-09-20 08:06:54 +0800

    tested id target for delete btn

--- src/components/PC-Part/Parts.js
+++ src/components/PC-Part/Parts.js
@@ -21,7 +21,7 @@ export default class Parts extends Component {
             <div className="card-part-item">
                 <div className="img-container" id={id}>
                     <img src={img} alt="" />
-                    <span className="close-btn">x</span>
+                    <span className="close-btn" onClick={() => removeItem(id)}>x</span>
 
                     <div className="item-info">
                         <h4>{partName}</h4>
